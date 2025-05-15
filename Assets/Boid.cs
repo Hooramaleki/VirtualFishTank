ceff4bd6e7c4cdeba1d01e312024e762a316bee7
Hooramaleki <huram1380@gmail.com>
2025-05-14 22:15:24 -0400

    Lab1

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -1,5 +1,58 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Boid : MonoBehaviour
 {
+
+    public GameObject targetObjest;
+    private Rigidbody Rigidbody;
+
+    public float speedMax = 2;
+    public float accelMax = 3;
+
+    private void Start()
+    {
+        //target
+        targetObjest = GameObject.Find("Target");
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+    
+    private void FixedUpdate()
+    {
+      
+        Vector3 toTarget = targetObjest.transform.position - transform.position;
+
+        Vector3 toTargetNormalized = toTarget.normalized;
+
+        Vector3 acceleration = toTargetNormalized * accelMax;
+
+        Rigidbody.linearVelocity += acceleration * Time.fixedDeltaTime;
+
+        Rigidbody.linearVelocity = Vector3.ClampMagnitude(Rigidbody.linearVelocity, speedMax);
+
+        transform.forward = Rigidbody.linearVelocity;
+    }
+
+
+    public new Rigidbody rigidbody;
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.linearVelocity = Random.insideUnitSphere;
+    }
+
+
+    private void Update()
+    {
+
+        AlignToVelocity();
+
+    }
+
+
+    public void AlignToVelocity()
+    {
+        transform.forward = Vector3.RotateTowards(transform.forward, rigidbody.linearVelocity.normalized, Mathf.Deg2Rad * 1800 * Time.deltaTime,100);
+
+    }
 }
--- Assets/BoidSimulation.cs
+++ Assets/BoidSimulation.cs
@@ -1,6 +1,66 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BoidSimulationControl : MonoBehaviour
 {
+
+    public GameObject boidPrefab = null;
+    public int boidToSpawn = 10;
+
+    public List<Boid> boids = null;
+
+    
+    public enum ControlMode
+    {
+        Seek,
+        Pursue,
+        Food,
+        Obstacle
+
+    }
+    public ControlMode controlMode = ControlMode.Seek;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+        // Spawn the boids
+        for (int i = 0; i < boidToSpawn; i++)
+        {
+            //the spoot of spawining
+            Vector3 position = new Vector3(Random.Range(0.7f,-1), Random.Range(0, 1.3f), Random.Range(-0.4f, 0));
+            Quaternion rotation = Random.rotation;
+
+            GameObject spawnerBoid = Instantiate(boidPrefab, position, rotation);
+            //randomizing the color
+            spawnerBoid.GetComponent<Renderer>().material.SetColor("_BaseColor", Random.ColorHSV(0,1,0.5f,1,0.5f,1));
+
+            spawnerBoid.GetComponent<Rigidbody>().linearVelocity = Random.insideUnitSphere * 0.3f;
+        }
+
+    }
+
+    //the react of diffrent keys/mouse keys
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            controlMode = ControlMode.Seek;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            controlMode = ControlMode.Pursue;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            controlMode = ControlMode.Food;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            controlMode = ControlMode.Obstacle;
+        }
+
+    }
 
 }
